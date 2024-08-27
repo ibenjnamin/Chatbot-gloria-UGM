576c44f3f3d22e5f72c36c3f066f3817d23ea6dc
Benjamin Darrouy <141375928+ibenjnamin@users.noreply.github.com>
2024-08-26 21:30:57 -0400

    Add files via upload

--- layout/config.js
+++ layout/config.js
@@ -1,21 +1,17 @@
 // layout/config.js
 require('dotenv').config(); // Cargar variables de entorno desde .env
 
-const mysql = require('mysql2');
-
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_DATABASE || 'ugm-gloria-ia',
+    host: process.env.DB_HOST, // Usa la variable de entorno correcta
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
-
-connection.connect((err) => {
+connection.connect(err => {
     if (err) {
         console.error('Error connecting to the database:', err);
         return;
     }
-    console.log('Connected to the MySQL server.');
+    console.log('Connected to the database.');
 });
-
-module.exports = connection;
