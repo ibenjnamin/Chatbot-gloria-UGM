d268c54eb2190baff3de025d4afc65962b999c46
Benjamin Darrouy <141375928+ibenjnamin@users.noreply.github.com>
2024-08-26 23:59:18 -0400

    Add files via upload

--- layout/config.js
+++ layout/config.js
@@ -1,17 +1,19 @@
-// layout/config.js
-require('dotenv').config(); // Cargar variables de entorno desde .env
-
 const mysql = require('mysql');
+
+// Creando la conexión usando variables de entorno, con fallback a valores predeterminados
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST, // Usa la variable de entorno correcta
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.DB_HOST || 'us-cluster-east-01.k8s.cleardb.net',
+  user: process.env.DB_USER || 'ba680eed0c64f0',
+  password: process.env.DB_PASSWORD || '03319b61',
+  database: process.env.DB_DATABASE || 'heroku_3f1d4ff0bfb4f0c'
 });
+
 connection.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database.');
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the database');
 });
+
+module.exports = connection;
